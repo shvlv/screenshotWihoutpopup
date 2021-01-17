5e0d114da29aae812ff285d729daa7f86a592c6b
taoufiq lotfi <leh.lotfi@gmail.com>
2021-01-17 09:59:20 +0200

    added comments

--- screenShot.js
+++ screenShot.js
@@ -10,12 +10,27 @@ const puppeteer = require("puppeteer");
     height: 1080,
     deviceScaleFactor: 1,
   });
+  // cleaning cookies just in case we alredy accepted the use of cookies before
   const client = await page.target().createCDPSession();
   await await client.send("Network.clearBrowserCookies");
-  await page.goto("https://github.com/tao101", {
-    waitUntil: "networkidle2",
-  });
 
-  await page.screenshot({ path: "test.png", fullPage: true });
+  await page.goto(
+    "https://www.politico.eu/article/us-regulator-authorizes-first-coronavirus-vaccine-for-emergency-use/",
+    {
+      //waiting until the page fully loades before doing anything
+      waitUntil: "domcontentloaded",
+    }
+  );
+
+  console.log("trying to find button");
+  //gree is the word in the popup Agree
+  const buttons = await page.$x("//a[contains(., 'gree')]");
+  console.log(buttons);
+  if (buttons.length > 0) {
+    console.log("found button");
+    await buttons[0].click();
+  }
+
+  await page.screenshot({ path: "politico after.png", fullPage: true });
   await browser.close();
 })();
