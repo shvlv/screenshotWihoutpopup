ebba25351e14e203c3b42e358713b44429d6421e
shvv <shvv86@gmail.com>
2021-01-18 15:34:59 +0200

    bulk screenshot

--- screenShot.js
+++ screenShot.js
@@ -1,5 +1,13 @@
 const puppeteer = require("puppeteer");
 
+/**
+ *
+ * @type []
+ */
+const data = require('./data.json');
+const fs = require('fs');
+let html = ['<ul>'];
+
 (async () => {
   const browser = await puppeteer.launch();
   const context = await browser.createIncognitoBrowserContext();
@@ -10,27 +18,49 @@ const puppeteer = require("puppeteer");
     height: 1080,
     deviceScaleFactor: 1,
   });
-  // cleaning cookies just in case we alredy accepted the use of cookies before
-  const client = await page.target().createCDPSession();
-  await await client.send("Network.clearBrowserCookies");
 
-  await page.goto(
-    "https://www.politico.eu/article/us-regulator-authorizes-first-coronavirus-vaccine-for-emergency-use/",
-    {
-      //waiting until the page fully loades before doing anything
-      waitUntil: "domcontentloaded",
+  data.map( async ({cappture, originalUrl}, index) => {
+    try {
+      // cleaning cookies just in case we alredy accepted the use of cookies before
+      const client = await page.target().createCDPSession();
+      await client.send("Network.clearBrowserCookies");
+      console.log(originalUrl);
+
+      await page.goto(
+          originalUrl,
+          {
+            //waiting until the page fully loades before doing anything
+            waitUntil: "domcontentloaded",
+          }
+      );
+
+      console.log("trying to find button");
+      //gree is the word in the popup Agree
+      const buttons = await page.$x("//a[contains(., 'gree')]");
+      console.log(buttons);
+      if (buttons.length > 0) {
+        console.log("found button");
+        await buttons[0].click();
+      }
+
+      let imgPath = `res/${index}.png`;
+      await page.screenshot({path: imgPath, fullPage: true});
+
+      html.push('<li>',
+          `<p>Cappture: <a href="${cappture}">${cappture}</a></p>`,
+          `<p>Original URL: <a href="${originalUrl}">${originalUrl}</a></p>`,
+          `<p>ScreenshotWithoutPopup: <img src="${imgPath}" alt="ScreenshotWithoutPopup"></p>`,
+          `</li>`,
+      );
+    } catch (e) {
+      console.log(originalUrl);
+      console.log(e);
     }
-  );
 
-  console.log("trying to find button");
-  //gree is the word in the popup Agree
-  const buttons = await page.$x("//a[contains(., 'gree')]");
-  console.log(buttons);
-  if (buttons.length > 0) {
-    console.log("found button");
-    await buttons[0].click();
-  }
+  });
 
-  await page.screenshot({ path: "politico after.png", fullPage: true });
   await browser.close();
+
+  html.push('</ul>');
+  fs.writeFileSync('res/result.html', html.join(''));
 })();
